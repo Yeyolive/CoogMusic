202f62de431bf1c2a6f17348924988f5cc601295
Armando Espinoza <earmando29@gmail.com>
2023-03-27 10:53:19 -0500

    Add project files.

---
+++ CoogMusic/Pages/Report/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoogMusic.Pages.Report
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
