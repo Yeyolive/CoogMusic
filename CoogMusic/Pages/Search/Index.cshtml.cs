21256601e7bd1ede20f6e9fef3b2a9bb2e8a8c55
earmando29 <earmando29@gmail.com>
2023-03-29 02:30:17 -0500

    Search Incomplete

--- CoogMusic/Pages/Search/Index.cshtml.cs
+++ CoogMusic/Pages/Search/Index.cshtml.cs
@@ -12,27 +12,49 @@ namespace CoogMusic.Pages.Search
         {
             try
             {
-                //using (MySqlConnection connection = new MySqlConnection(connectionString))
-                //{
-                //    connection.Open();
-                //    String sql = "SELECT * FROM songs";
-                //    using (MySqlCommand command = new MySqlConnection(sql, connection))
-                //    {
-                //        SongInfo songInfo = new SongInfo();
-                //        songInfo.title = reader.GetString(1);
-                //    }
-                //}
+                String connectionStr = "Server=coogmusic.mysql.database.azure.com;User ID=qalksktvpv;Password=coogmusic1!;Database=coogmusicdb";
+                using (MySqlConnection connection = new MySqlConnection(connectionStr))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM song AS S";
+                    using (MySqlCommand command = new MySqlCommand(sql, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                SongInfo songInfo = new SongInfo();
+                                songInfo.title = reader.GetString(2);
+                                songInfo.genre = reader.GetString(3);
+                                songInfo.CreateDate = reader.GetDateTime(6).ToString();
+
+                                listSongs.Add(songInfo);
+                            }
+                        }
+                    }
+                }
             }
             catch (Exception ex)
             {
 
             }
         }
-        public class SongInfo
+        public async Task OnPostAsync()
         {
-            public String title;
-            public String artist;
-            public String album;
+
         }
+    }
+    public class SongInfo
+    {
+        public String RecordLabel;
+        public String CreateDate;
+        public String Name;
+        public int songId;
+        public int userId;
+        public int? artistId;
+        public String artist;
+        public String genre;
+        public String title;
+        public IFormFile songFile;
     }
 }
