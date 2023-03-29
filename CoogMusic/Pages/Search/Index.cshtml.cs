ab553bdb4d7ce8889b5dcdd6ed66a370dc57e266
earmando29 <earmando29@gmail.com>
2023-03-29 17:13:48 -0500

    Almost there

--- CoogMusic/Pages/Search/Index.cshtml.cs
+++ CoogMusic/Pages/Search/Index.cshtml.cs
@@ -10,7 +10,7 @@ namespace CoogMusic.Pages.Search
     {
         public List<SongView> listSongs = new List<SongView>();
         List<String> urls = new List<String>();
-        public IActionResult Search(String q)
+        public void OnPost()
         {
             try
             {
@@ -21,7 +21,7 @@ namespace CoogMusic.Pages.Search
                     String sql = "SELECT * FROM song AS S WHERE name LIKE @song_searched";
                     using (MySqlCommand command = new MySqlCommand(sql, connection))
                     {
-                        command.Parameters.AddWithValue("@SearchTerm", "%" + q + "%");
+                        command.Parameters.AddWithValue("@SearchTerm", "%" + "NEED VALUE FROM SEARCH INPUT" + "%");
                         using (MySqlDataReader reader = command.ExecuteReader())
                         {
                             while (reader.Read())
@@ -32,25 +32,47 @@ namespace CoogMusic.Pages.Search
                                 songInfo.data = (byte[])reader["track"];
 
                                 listSongs.Add(songInfo);
-                                urls.Add($"data:audio/mp3;base64,{Convert.ToBase64String(songInfo.data)}");
                             }
                         }
                     }
                 }
-                return Page();
             }
             catch (Exception ex)
             {
-                return Page();
             }
         }
-        public async Task OnPostAsync()
+        public IActionResult PlaySong(int id)
         {
-
+            // Retrieve the BLOB data for the song with the specified ID
+            byte[] songData;
+            String connectionStr = "Server=coogmusic.mysql.database.azure.com;User ID=qalksktvpv;Password=coogmusic1!;Database=coogmusicdb";
+            using (MySqlConnection connection = new MySqlConnection(connectionStr))
+            {
+                connection.Open();
+                String sql = "SELECT track FROM song WHERE id = @Id";
+                using (MySqlCommand command = new MySqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            songData = (byte[])reader["track"];
+                        }
+                        else
+                        {
+                            return NotFound();
+                        }
+                    }
+                }
+            }
+            // Return the BLOB data as a file with the correct MIME type
+            return File(songData, "audio/mpeg");
         }
     }
     public class SongView
     {
+        public int songId;
         public String title;
         public String genre;
         public byte[] data;
