653f8f63ee3f535a8e29b0ae36088426f4ce367e
earmando29 <earmando29@gmail.com>
2023-03-31 15:37:38 -0500

    Removed old web builder

--- CoogMusic/Program.cs
+++ CoogMusic/Program.cs
@@ -38,19 +38,19 @@ using MySql.Data.MySqlClient;
     app.MapRazorPages();
 
     app.Run();
-namespace CoogMusic{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-              .UseKestrel()
-              .UseContentRoot(Directory.GetCurrentDirectory())
-              .UseIISIntegration()
-              .UseStartup<Startup>()
-              .Build();
+//namespace CoogMusic{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            var host = new WebHostBuilder()
+//              .UseKestrel()
+//              .UseContentRoot(Directory.GetCurrentDirectory())
+//              .UseIISIntegration()
+//              .UseStartup<Startup>()
+//              .Build();
 
-            host.Run();
-        }
-    }
-}
+//            host.Run();
+//        }
+//    }
+//}
