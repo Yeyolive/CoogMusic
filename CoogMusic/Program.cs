77b70522678c75c813addc5d368542b0900730e3
earmando29 <earmando29@gmail.com>
2023-03-31 17:28:19 -0500

    Remove localhost stuff

--- CoogMusic/Program.cs
+++ CoogMusic/Program.cs
@@ -21,17 +21,21 @@ builder.WebHost.UseUrls("http://*:8080");
 
 var app = builder.Build();
 
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
     {
-        app.UseMigrationsEndPoint();
+        app.UseHttpsRedirection();
     }
-    else
-    {
-        app.UseExceptionHandler("/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
+    //if (app.Environment.IsDevelopment())
+    //{
+    //    app.UseMigrationsEndPoint();
+    //}
+    //else
+    //{
+    //    app.UseExceptionHandler("/Error");
+    //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //    app.UseHsts();
+    //}
 
     app.UseHttpsRedirection();
     app.UseStaticFiles();
