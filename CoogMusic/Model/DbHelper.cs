717564bb3b1729f4082d32d449fd11b0f52169c5
earmando29 <earmando29@gmail.com>
2023-04-01 18:45:46 -0500

    Added Functions For Creating User

--- CoogMusic/Model/DbHelper.cs
+++ CoogMusic/Model/DbHelper.cs
@@ -4,49 +4,122 @@ using System.Data;
 using Azure;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using System.Reflection.PortableExecutable;
 
 namespace CoogMusic.Pages
 {
 	public class DbHelper
     {
         String connectionString = "Server=coogmusic.mysql.database.azure.com;User ID=qalksktvpv;Password=coogmusic1!;Database=coogmusicdb";
-        public async Task CreateUser(ApplicationUser user, String password)
+        public async Task CreateLogin(ApplicationUser user, String password, String userType)
         {
-            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            try
             {
-                await conn.OpenAsync();
-                MySqlTransaction mySqlTransaction = conn.BeginTransaction();
-                String sql = "INSERT INTO login (email, passwrd) VALUES (@Email, @Password)";
-                using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
                 {
-                    cmd.Parameters.AddWithValue("@Email", user.Email);
-                    cmd.Parameters.AddWithValue("@Password", password);
-                    int affectedRows = await cmd.ExecuteNonQueryAsync();
+                    await conn.OpenAsync();
+
+                    MySqlTransaction mySqlTransaction = conn.BeginTransaction();
+                    String sql = "INSERT INTO login (email, passwrd) VALUES (@Email, @Password)";
+                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Email", user.Email);
+                        cmd.Parameters.AddWithValue("@Password", password);
+                        int affectedRows = await cmd.ExecuteNonQueryAsync();
+                    }
+
+                    mySqlTransaction.Commit();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write("ERROR Writing to Database: " + ex.ToString());
+            }
+        }
+
+        public async Task CreateUser(ApplicationUser user, String userType)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    await conn.OpenAsync();
+
+                    MySqlTransaction mySqlTransaction = conn.BeginTransaction();
                     
-                    await cmd.ExecuteNonQueryAsync();
+                    String sql = "INSERT INTO users (name, email, mobile, sex, age) VALUES (@Name, @Email, @Mobile, @Sex, @Age)";
+                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Name", user.Name);
+                        cmd.Parameters.AddWithValue("@Email", user.Email);
+                        cmd.Parameters.AddWithValue("@Mobile", user.Mobile);
+                        cmd.Parameters.AddWithValue("@Sex", user.Sex);
+                        cmd.Parameters.AddWithValue("@Age", user.Age);
+                        int affectedRows = await cmd.ExecuteNonQueryAsync();
+                    }
+                    mySqlTransaction.Commit();
                 }
+            }
+            catch(Exception ex)
+            {
+                Console.Write("ERROR Writing to Database: " + ex.ToString());
+            }
+        }
 
-                sql = "INSERT INTO users (name, email, mobile, create_date, sex, age) VALUES (@Name, @Email, @Mobile, @CreateDate, @Sex, @Age)";
-                using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+        public async Task CreateArtistOrListener(ApplicationUser user, String userType)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
                 {
-                    cmd.Parameters.AddWithValue("@Name", user.Name);
-                    cmd.Parameters.AddWithValue("@Email", user.Email);
-                    cmd.Parameters.AddWithValue("@Mobile", user.Mobile);
-                    cmd.Parameters.AddWithValue("@CreateDate", user.CreateDate);
-                    cmd.Parameters.AddWithValue("@Sex", user.Sex);
-                    cmd.Parameters.AddWithValue("@Age", user.Age);
-                    int affectedRows = await cmd.ExecuteNonQueryAsync();
+                    await conn.OpenAsync();
 
-                    await cmd.ExecuteNonQueryAsync();
+                    MySqlTransaction mySqlTransaction = conn.BeginTransaction();
+                    String sql = "SELECT id FROM user WHERE email=@Email";
+                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Email", user.Email);
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                user.Id = reader.GetInt32("id");
+                            }
+                        }
+                    }
+                        if (userType == "Artist")
+                        {
+                            sql = "INSERT INTO artist (user_id, name, record_label) VALUES (@UserId, @Name, @RecordLabel)";
+                            using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                            {
+                                cmd.Parameters.AddWithValue("@Name", user.Name);
+                                cmd.Parameters.AddWithValue("@RecordLabel", user.recordLabel);
+                                cmd.Parameters.AddWithValue("@UserId", user.Id);
+                                int affectedRows = await cmd.ExecuteNonQueryAsync();
+                            }
+                        }
+                        else
+                        {
+                            sql = "INSERT INTO listener (name, id) VALUES (@Name, @UserId)";
+                            using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                            {
+                                cmd.Parameters.AddWithValue("@Name", user.Name);
+                                cmd.Parameters.AddWithValue("@UserId", user.Email);
+                                int affectedRows = await cmd.ExecuteNonQueryAsync();
+                            }
+                        }
+
+                    mySqlTransaction.Commit();
                 }
-                mySqlTransaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                Console.Write("ERROR Writing to Database: " + ex.ToString());
             }
         }
 
         public async Task<ApplicationUser> GetUserByEmailAndPassword(String email, String pwd)
         {
-            String connectionString = "Server=coogmusic.mysql.database.azure.com;User ID=qalksktvpv;Password=coogmusic1!;Database=coogmusicdb";
-
             using (MySqlConnection conn = new MySqlConnection(connectionString))
             {
                 await conn.OpenAsync();
@@ -79,14 +152,15 @@ namespace CoogMusic.Pages
 
         public async Task<bool> IsUserArtist(int userId)
         {
-            String connectionString = "Server=coogmusic.mysql.database.azure.com;User ID=qalksktvpv;Password=coogmusic1!;Database=coogmusicdb";
             int count = 0;
             using (MySqlConnection conn = new MySqlConnection(connectionString))
             {
                 await conn.OpenAsync();
-                String sql = $"SELECT COUNT(*) FROM artist WHERE user_id={userId};";
+                String sql = "SELECT COUNT(*) FROM artist WHERE user_id=@UserId;";
                 using (MySqlCommand cmd = new MySqlCommand(sql, conn))
                 {
+                    cmd.Parameters.AddWithValue("@UserId", userId);
+
                     count = int.Parse(cmd.ExecuteScalar().ToString());
                 }
             }
@@ -95,18 +169,36 @@ namespace CoogMusic.Pages
 
         public async Task<bool> IsUserListener(int userId)
         {
-            String connectionString = "Server=coogmusic.mysql.database.azure.com;User ID=qalksktvpv;Password=coogmusic1!;Database=coogmusicdb";
             int count = 0;
             using (MySqlConnection conn = new MySqlConnection(connectionString))
             {
                 await conn.OpenAsync();
-                String sql = $"SELECT COUNT(*) FROM listener WHERE id={userId};";
+                String sql = "SELECT COUNT(*) FROM listener WHERE id=@UserId;";
                 using (MySqlCommand cmd = new MySqlCommand(sql, conn))
                 {
+                    cmd.Parameters.AddWithValue("@UserId", userId);
+
                     count = int.Parse(cmd.ExecuteScalar().ToString());
                 }
             }
             return count > 0;
+        }
+
+        public async Task<bool> EmailExists(string email)
+        {
+            int count = 0;
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                await conn.OpenAsync();
+                String sql = "SELECT COUNT(*) FROM login WHERE email=@Email;";
+                using (var cmd = new MySqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Email", email);
+
+                    count = int.Parse(cmd.ExecuteScalar().ToString());
+                    return count > 0;
+                }
+            }
         }
     }
 }
