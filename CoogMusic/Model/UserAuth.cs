ef1db9355efb70a23343bc58431018ee7ad483b2
earmando29 <earmando29@gmail.com>
2023-04-01 11:24:49 -0500

    Added Class

---
+++ CoogMusic/Model/UserAuth.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace CoogMusic.Pages
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? Name { get; set; }
+        public string? Mobile { get; set; }
+        public DateTime CreateDate { get; set; }
+        public char Sex { get; set; }
+        public int Age { get; set; }
+
+        // Navigation properties for listener and artist
+        public Listener? Listener { get; set; }
+        public Artist? Artist { get; set; }
+    }
+
+    public class Listener
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        // Navigation property for user
+        public ApplicationUser? User { get; set; }
+    }
+
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string? Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string? Description { get; set; }
+        public string? RecordLabel { get; set; }
+
+        // Navigation property for user
+        public ApplicationUser? User { get; set; }
+    }
+}
+
