5b175bbca8119c0ac02cc896a4497422f41e4158
earmando29 <earmando29@gmail.com>
2023-04-01 15:50:23 -0500

    Working Login

--- CoogMusic/Pages/Account/LoginTwo.cshtml.cs
+++ CoogMusic/Pages/Account/LoginTwo.cshtml.cs
@@ -9,8 +9,36 @@ namespace CoogMusic.Pages.Account
 {
 	public class LoginTwoModel : PageModel
     {
-        public void OnGet()
+        private readonly DbHelper _databaseHelper;
+
+        public LoginTwoModel(IConfiguration configuration)
         {
+            _databaseHelper = new DbHelper();
+        }
+        public string ErrorMessage { get; set; }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            ApplicationUser login = new ApplicationUser();
+            login.Email = Request.Form["Input-Email"];
+            login.Password = Request.Form["Input-Password"];
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ApplicationUser user = await _databaseHelper.GetUserByEmailAndPassword(login.Email, login.Password);
+
+            if (user != null)
+            {
+                // Add the login logic here, e.g., sign in the user
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                ErrorMessage = "Invalid email or password";
+                return Page();
+            }
         }
     }
 }
