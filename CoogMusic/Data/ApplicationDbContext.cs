e16230ec5452c6b752957a735a2a2220291f2078
earmando29 <earmando29@gmail.com>
2023-04-01 20:55:28 -0500

    Fix SIte Down

--- CoogMusic/Data/ApplicationDbContext.cs
+++ CoogMusic/Data/ApplicationDbContext.cs
@@ -10,19 +10,7 @@ namespace CoogMusic.Data
             : base(options)
         {
         }
-        public DbSet<Login> Logins { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Other model configurations...
-
-            modelBuilder.Entity<Listener>()
-                .HasOne(l => l.User)
-                .WithOne(u => u.Listener)
-                .HasForeignKey<Listener>(l => l.Id); // Assuming Listener has a UserId foreign key property
-        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
     }
 
