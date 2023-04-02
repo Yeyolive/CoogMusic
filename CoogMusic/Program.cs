74d38af445d3782734c133546ec8a82a99b18966
earmando29 <earmando29@gmail.com>
2023-04-01 19:40:56 -0500

    Fix Site Down

--- CoogMusic/Program.cs
+++ CoogMusic/Program.cs
@@ -26,18 +26,7 @@ var app = builder.Build();
     {
         app.UseHttpsRedirection();
     }
-    //if (app.Environment.IsDevelopment())
-    //{
-    //    app.UseMigrationsEndPoint();
-    //}
-    //else
-    //{
-    //    app.UseExceptionHandler("/Error");
-    //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    //    app.UseHsts();
-    //}
 
-    app.UseHttpsRedirection();
     app.UseStaticFiles();
 
     app.UseRouting();
