d1dd5986d1d887ab3df5a5a6468b14581ff22edd
earmando29 <earmando29@gmail.com>
2023-04-03 23:04:58 -0500

    Added Logout Functionality

---
+++ CoogMusic/Pages/Account/LogOutTwo.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoogMusic.Pages.Account
+{
+	public class LogOutTwoModel : PageModel
+    {
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return LocalRedirect(returnUrl ?? "/");
+        }
+
+    }
+}
