abf7acf07ebb5840067bb988e46e4955cc04848c
earmando29 <earmando29@gmail.com>
2023-04-10 14:43:13 -0500

    Added JS functionality

---
+++ CoogMusic/wwwroot/js/search.js
@@ -0,0 +1,16 @@
+﻿function addToPlaylist(songId) {
+    var playlistId = ...; // Fetch the playlist ID for the current user from the server
+    var url = "/AddToPlaylist?songId=" + songId + "&playlistId=" + playlistId;
+    fetch(url, { method: "POST" })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert("Song added to the playlist!");
+            } else {
+                alert("Error adding song to the playlist.");
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+}
