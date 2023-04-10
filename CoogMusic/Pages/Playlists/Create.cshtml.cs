a01c30e886fc88d919b0e961b9aa16f82e0e1c64
earmando29 <earmando29@gmail.com>
2023-04-10 14:02:01 -0500

    New Playlist Working

--- CoogMusic/Pages/Playlists/Create.cshtml.cs
+++ CoogMusic/Pages/Playlists/Create.cshtml.cs
@@ -4,29 +4,69 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace CoogMusic.Pages.Playlists
 {
 	public class CreateModel : PageModel
     {
-        private readonly IConfiguration _configuration;
+        private readonly DbHelper _databaseHelper;
+        private readonly string connectionStr;
 
         public CreateModel(IConfiguration configuration)
         {
-            _configuration = configuration;
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            connectionStr = connectionString;
+            _databaseHelper = new DbHelper(connectionString);
         }
 
         [BindProperty]
         public PlaylistInfo NewPlaylist { get; set; }
 
-        public void OnGet()
-        {
-        }
+        public String successMessage = "";
+        public String errorMessage = "";
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(String ListenerId, String Title, String Description)
         {
             // ... add code to insert a new playlist into the database ...
-            return RedirectToPage("/MyPlaylists");
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionStr))
+                {
+                    await connection.OpenAsync();
+                    MySqlTransaction mySqlTransaction = connection.BeginTransaction();
+                    String sql = "INSERT INTO playlist (user_id, title, description) VALUES (@UserId, @Title, @Desc);";
+                    using (MySqlCommand command = new MySqlCommand(sql, connection))
+                    {
+                        command.Transaction = mySqlTransaction;
+
+                        command.Parameters.AddWithValue("@UserId", ListenerId);
+                        command.Parameters.AddWithValue("@Title", Title);
+                        command.Parameters.AddWithValue("@Desc", Description);
+
+                        int affectedRows = await command.ExecuteNonQueryAsync();
+                        if (affectedRows > 0)
+                        {
+                            // Display a success message or redirect to another page
+                            successMessage = "New Playlists Created";
+                            Response.Redirect("/Playlists/");
+                        }
+                        else
+                        {
+                            errorMessage = "We experienced an error while adding to the database";
+                        }
+                        mySqlTransaction.Commit();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = "We experienced an error while adding to the database";
+                Console.WriteLine("Error inserting MP3 file into database: " + ex.Message);
+            }
+            return Page();
         }
     }
 }
