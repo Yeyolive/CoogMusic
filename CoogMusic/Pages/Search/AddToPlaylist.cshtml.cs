610d9e695c31ef0cec5f25b390664224a91f81f8
earmando29 <earmando29@gmail.com>
2023-04-10 18:15:58 -0500

    Removed functions

--- CoogMusic/Pages/Search/AddToPlaylist.cshtml.cs
+++ CoogMusic/Pages/Search/AddToPlaylist.cshtml.cs
@@ -5,50 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Security.Claims;
 
 namespace CoogMusic.Pages.Search
 {
     public class AddToPlaylistModel : PageModel
     {
-        private readonly DbHelper _dbHelper; // Replace CoogMusicDbContext with your actual DbContext class name
+        private readonly IConfiguration _configuration;
 
         public AddToPlaylistModel(IConfiguration configuration)
         {
-            string connectionString = configuration.GetConnectionString("DefaultConnection");
-            _dbHelper = new DbHelper(connectionString);
+            _configuration = configuration;
         }
 
-        [BindProperty(SupportsGet = true)]
-        public int SongId { get; set; }
-
-        [BindProperty(SupportsGet = true)]
-        public int PlaylistId { get; set; }
-
-        public bool Success { get; set; }
-
-        //public async Task<IActionResult> OnPostAddSongToPlaylist(PlaylistInfo viewModel)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    var connectionString = _configuration.GetConnectionString("DefaultConnection");
-
-        //    using (var connection = new MySqlConnection(connectionString))
-        //    {
-        //        var query = "INSERT INTO playlist_songs (playlist_id, song_id, song_title, artist_name) VALUES (@PlaylistId, @SongId, @SongTitle, @ArtistName)";
-        //        var affectedRows = await connection.ExecuteAsync(query, viewModel);
-
-        //        if (affectedRows > 0)
-        //        {
-        //            return Ok();
-        //        }
-        //        else
-        //        {
-        //            return StatusCode(500);
-        //        }
-        //    }
-        //}
+        
     }
 }
