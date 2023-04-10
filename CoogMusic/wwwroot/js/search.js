65587dd5d82e84089809bb94dd5969a8e6b6f8f5
earmando29 <earmando29@gmail.com>
2023-04-10 16:41:00 -0500

    Updated to Display Title

--- CoogMusic/wwwroot/js/search.js
+++ CoogMusic/wwwroot/js/search.js
@@ -1,16 +1,58 @@
-﻿function addToPlaylist(songId) {
-    var playlistId = ...; // Fetch the playlist ID for the current user from the server
-    var url = "/AddToPlaylist?songId=" + songId + "&playlistId=" + playlistId;
-    fetch(url, { method: "POST" })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                alert("Song added to the playlist!");
-            } else {
-                alert("Error adding song to the playlist.");
-            }
-        })
-        .catch(error => {
-            console.error("Error:", error);
+﻿function addToPlaylist(songId, songTitle, artistName) {
+    // Show the modal
+    var modal = document.getElementById("playlist-modal");
+    modal.style.display = "block";
+
+    // Load user's playlists and display them
+    $.get("/Search?handler=UserPlaylists", function (data) {
+            var container = document.getElementById("playlists-container");
+            container.innerHTML = '';
+            data.forEach(playlist => {
+                console.log('Playlist:', playlist);
+
+                //var button = document.createElement('button');
+                //button.classList.add('btn', 'btn-primary', 'btn-action');
+                //button.textContent = playlist.Title;
+                //button.onclick = function () {
+
+                var listItem = document.createElement('li');
+                listItem.textContent = playlist.title;
+                listItem.style.cursor = 'pointer';
+                listItem.onclick = function () {
+                    // Add the song to the selected playlist
+                    fetch('/Search?handler=AddToPlaylist', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'RequestVerificationToken': document.getElementsByName('__RequestVerificationToken')[0].value
+                        },
+                        body: JSON.stringify({ playlistId: playlist.id, songId: songId, songTitle: songTitle, artistName: artistName })
+                    })
+                        .then(response => {
+                            if (response.ok) {
+                                alert('Song added to the playlist!');
+                            } else {
+                                alert('Failed to add the song to the playlist.');
+                            }
+                        });
+
+                    // Close the modal
+                    modal.style.display = "none";
+                };
+                //container.appendChild(button);
+                container.appendChild(listItem);
+            });
         });
 }
+
+// Add the modal script
+var modal = document.getElementById("playlist-modal");
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function () {
+    modal.style.display = "none";
+};
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
