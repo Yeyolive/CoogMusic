7bd179c8a0bdb698145cafa8a788c004b1dbe42f
earmando29 <earmando29@gmail.com>
2023-04-10 12:00:11 -0500

    Init Playlist

---
+++ CoogMusic/Pages/Playlists/Create.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoogMusic.Pages.Playlists
+{
+	public class CreateModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        public CreateModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [BindProperty]
+        public PlaylistInfo NewPlaylist { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            // ... add code to insert a new playlist into the database ...
+            return RedirectToPage("/MyPlaylists");
+        }
+    }
+}
---
+++ CoogMusic/Pages/Playlists/PlaylistSongs.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoogMusic.Pages.Playlists
+{
+	public class PlaylistSongsModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+        public List<SongView> PlaylistSongs { get; set; }
+
+        public PlaylistSongsModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task OnGetAsync(int playlistId)
+        {
+            // ... add code to retrieve the songs in the specified playlist from the database ...
+        }
+    }
+}
