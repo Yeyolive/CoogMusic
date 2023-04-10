21cdaaafc0f3db26e0b46530429c2fdeed7027ef
earmando29 <earmando29@gmail.com>
2023-04-10 14:02:43 -0500

    Filled Class

--- CoogMusic/Model/PlaylistInfo.cs
+++ CoogMusic/Model/PlaylistInfo.cs
@@ -3,5 +3,11 @@ using CoogMusic.Pages;
 
 public class PlaylistInfo
 {
+    public int Id;
+    public int UserId;
+    public int SongCount;
+    public String? Title;
+    public String? Description;
+    public Boolean? Deleted;
+    public TimeSpan? Duration;
 }
-
