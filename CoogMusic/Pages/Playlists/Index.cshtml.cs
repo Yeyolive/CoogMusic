7c89fd7ea28017a5de67b9e3132b42885112fb62
earmando29 <earmando29@gmail.com>
2023-04-10 11:45:22 -0500

    Initializing Playlists

---
+++ CoogMusic/Pages/Playlists/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using CoogMusic.Model;
+
+namespace CoogMusic.Pages.Playlists
+{
+	public class IndexModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+        public List<PlaylistInfo> UserPlaylists { get; set; }
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task OnGetAsync()
+        {
+            // ... add code to retrieve the user's playlists from the database ...
+        }
+    }
+}
---
+++ CoogMusic/Pages/Search/AddToPlaylist.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using CoogMusic.Model;
+
+namespace CoogMusic.Pages.Search
+{
+    public class AddToPlaylistModel : PageModel
+    {
+        private readonly DbHelper _dbHelper; // Replace CoogMusicDbContext with your actual DbContext class name
+
+        public AddToPlaylistModel(IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            _dbHelper = new DbHelper(connectionString);
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public int SongId { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int PlaylistId { get; set; }
+
+        public bool Success { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            try
+            {
+                // ...
+                // The rest of the method implementation
+                // ...
+
+                Success = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Success = false;
+            }
+
+            return Page();
+        }
+    }
+}
