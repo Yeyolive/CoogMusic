141ccd01ccdb4159e452f208d5b448df2c3e125f
vuong1510 <128824296+vuong1510@users.noreply.github.com>
2023-04-13 13:27:05 -0500

    History report (not done yet)

---
+++ CoogMusic/Model/history.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+using CoogMusic.Pages;
+    public class Historyofuser
+    {
+        public int? songId;
+        public int? userId;
+        public int artistId;
+        public String? genre;
+        public String? last_played;
+        public String? artist;
+        public String? title;
+
+    }
