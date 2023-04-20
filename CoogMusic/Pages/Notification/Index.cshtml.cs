9ffa59ed867364d059ca3b7908ca9a547ee5ad4c
earmando29 <earmando29@gmail.com>
2023-04-20 16:15:35 -0500

    Added Notification Page

---
+++ CoogMusic/Pages/Notification/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoogMusic.Pages.Notification
+{
+	public class IndexModel : PageModel
+    {
+        public String errorMessage = "";
+        public String successMessage = "";
+        public SongInfo songInfo = new SongInfo();
+
+        private readonly DbHelper _databaseHelper;
+        // Access this by calling connectionStr variable in your functions (GLOBAL)
+        private readonly string connectionStr;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            connectionStr = connectionString;
+            _databaseHelper = new DbHelper(connectionString);
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
