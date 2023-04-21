504527e890e8d7bda4d79dd0a5448a0967bebf8b
Vian <89276801+vpluu@users.noreply.github.com>
2023-04-21 18:22:58 -0500

    took off redirection js

--- CoogMusic/wwwroot/js/site.js
+++ CoogMusic/wwwroot/js/site.js
@@ -1,10 +1,1 @@
-﻿function goToNotificationsPage() {
-    window.location.href = '/Notification';
-}
-
-$(document).ready(function () {
-    $('#notificationBell').on('click', function () {
-        goToNotificationsPage();
-    });
-});
-
+﻿
