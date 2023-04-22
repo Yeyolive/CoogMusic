efc1381835ac4b712a2a9097850c0e1fdfc4f6c4
earmando29 <earmando29@gmail.com>
2023-04-22 11:46:00 -0500

    Display Notifications working Removed JS Updated C# function (not returning JSON)

--- CoogMusic/Pages/Notification/Index.cshtml.cs
+++ CoogMusic/Pages/Notification/Index.cshtml.cs
@@ -24,6 +24,7 @@ namespace CoogMusic.Pages.Notification
         private readonly string connectionStr;
         public int UserId => int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
 
+        public List<NotificationData> notifications = new List<NotificationData>();
 
         public IndexModel(IConfiguration configuration)
         {
@@ -32,31 +33,30 @@ namespace CoogMusic.Pages.Notification
             _databaseHelper = new DbHelper(connectionString);
         }
 
-        [HttpGet]
-        public JsonResult OnGetDisplayNotifications()
+        public async void OnGetAsync()
         {
-            List<NotificationData> notifications = new List<NotificationData>();
+            int artistId = await _databaseHelper.GetArtistIdByUserId(int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)));
+
             using (MySqlConnection connection = new MySqlConnection(connectionStr))
             {
                 connection.Open();
-                string sql = "SELECT * FROM notification WHERE artist_id = @userId";
+                string sql = "SELECT message FROM notification WHERE artist_id = @ArtistId";
                 using (MySqlCommand command = new MySqlCommand(sql, connection))
                 {
-                    command.Parameters.AddWithValue("@userId", UserId);
+                    command.Parameters.AddWithValue("@ArtistId", artistId);
                     using (MySqlDataReader reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
                             NotificationData notification = new NotificationData();
-                            /*notification.Message = reader["message"].ToString();
-                            */
-                            notification.Message = "inside c# fcunti djf";
+                            notification.Message = reader["message"].ToString();
+                            Console.WriteLine(notification.Message);
+                            //notification.Message = "inside c# fcunti djf";
                             notifications.Add(notification);
                         }
                     }
                 }
             }
-            return new JsonResult(notifications);
         }
 
 
@@ -64,7 +64,7 @@ namespace CoogMusic.Pages.Notification
 
         public class NotificationData
         {
-            public string Message { get; set; }
+            public string? Message { get; set; }
         }
     }
 }
